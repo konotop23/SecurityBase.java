4e8500d0c081141c51b9ea8bf7519b55032097b4
konotop23 <konotopchenko123@gmail.com>
2021-09-28 16:25:05 +0300

    Create a new DataBase

--- src/main/java/ConnectionManager.java
+++ src/main/java/ConnectionManager.java
@@ -1,9 +1,9 @@
 import java.sql.*;
 
 public final class ConnectionManager {
-    private static final String PASSWORD_KEY = "db.password";
-    private static final String USERNAME_KEY = "db.username";
-    private static final String URL_KEY = "db.url";
+    private static final String PASSWORD_KEY = "db.password2";
+    private static final String USERNAME_KEY = "db.username2";
+    private static final String URL_KEY = "db.url2";
 
     public ConnectionManager() {
     }
@@ -14,7 +14,6 @@ public final class ConnectionManager {
                     PropertiesUtil.get(URL_KEY),
                     PropertiesUtil.get(USERNAME_KEY),
                     PropertiesUtil.get(PASSWORD_KEY));
-
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
